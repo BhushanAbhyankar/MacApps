f1c711c0f68aebf88cb254e0175ec45f76090f77
EdwardDuc <edward.duclervil@techconsulting.tech>
2023-08-17 09:30:34 -0400

    Merge conflict resolved?

--- MacOSSwiftUI/MacOSSwiftUI/ContentView.swift
+++ MacOSSwiftUI/MacOSSwiftUI/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("the item is inputted at \(item.timestamp!, formatter: itemFormatter)").accessibilityHint("This is some extra Info")
+                        Text("the item is inputted at \(item.timestamp ?? .now, formatter: itemFormatter)").accessibilityHint("This is some info")
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
